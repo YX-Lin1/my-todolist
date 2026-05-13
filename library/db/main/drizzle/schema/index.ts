77929abfda0765604128a0ba73d63cddb2e2b695
YX-Lin1 <yuxuan.lin1@gientech.com>
2026-05-13 17:50:32 +0900

    Add todoTable schema to main schema and export it

--- library/db/main/drizzle/schema/index.ts
+++ library/db/main/drizzle/schema/index.ts
@@ -4,3 +4,4 @@ export { mainSchema as publicSchema } from "./main-schema";
 export { ordersTable } from "./main-schema/orders-table";
 export { productsTable } from "./main-schema/products-table";
 export { usersTable } from "./main-schema/users-table";
+export { todoTable } from "./main-schema/todolists-table";
---
+++ library/db/main/drizzle/schema/main-schema/todolists-table.ts
@@ -0,0 +1,12 @@
+import { boolean, timestamp, uuid, varchar } from "drizzle-orm/pg-core";
+import { usersTable } from "./users-table";
+import { mainSchema } from "../main-schema";
+
+export const todoTable = mainSchema.table("todolists", {
+  id: uuid("id").primaryKey(),
+  user_id:uuid("user_id").references(() => usersTable.id),
+  todo:varchar("todo", { length: 255 }).notNull(),
+  completed:boolean("completed").notNull().default(false),
+  created_at:timestamp("created_at").defaultNow(),
+  updated_at:timestamp("updated_at").defaultNow(),
+});
