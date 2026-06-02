92375fa7d136ac84e10ba35fa01668eae1d91623
YX-Lin1 <yuxuan.lin1@gientech.com>
2026-06-02 11:50:02 +0900

    feat(router): Integrate login router into the main application router

--- library/trpc/routers/index.ts
+++ library/trpc/routers/index.ts
@@ -1,10 +1,12 @@
 import { router } from "../trpc";
 import { usersRouter } from "./users/users-router";
 import { todolistsRouter } from "./todolists/todolists-router";
+import { loginRouter } from "./login/login-router";
 
 export const appRouter = router({
   users: usersRouter,
   todolists: todolistsRouter,
+  login: loginRouter,
 });
 
 export type AppRouter = typeof appRouter;
