ce2bcd39213759e05425865e2b1782a62f06ac14
YX-Lin1 <yuxuan.lin1@gientech.com>
2026-06-05 10:34:14 +0900

    refactor(schema): Remove orders and products repositories from mainDbMap, add sessions repository implementation

--- library/db/main/registrations.ts
+++ library/db/main/registrations.ts
@@ -1,25 +1,21 @@
 import { defineResources } from "@surgeteam/di/define-resources";
 import type { ContainerFromMap, ResolverShape } from "@surgeteam/di/types";
 import { createMainDbClient } from "./client";
-import type { OrdersRepository } from "./orders/orders-repository";
-import { OrdersRepositoryImpl } from "./orders/orders-repository-impl";
-import type { ProductsRepository } from "./products/products-repository";
-import { ProductsRepositoryImpl } from "./products/products-repository-impl";
 import type { UsersRepository } from "./users/users-repository";
 import { UsersRepositoryImpl } from "./users/users-repository-impl";
 import type {TodoRepository} from "./todolists/todolists-repository";
 import {TodoRepositoryImpl} from "./todolists/todolists-repository-impl";
+import type {SessionsRepository} from "./sessions/sessions-repository";
+import {SessionsRepositoryImpl} from "./sessions/sessions-repository-impl";
 
 const mainDbMap = {
   MainDbClient: () => createMainDbClient(),
   UsersRepository: (resolver: ResolverShape): UsersRepository =>
     new UsersRepositoryImpl(resolver.get(MainDbTokens.mainDbClient)),
-  OrdersRepository: (resolver: ResolverShape): OrdersRepository =>
-    new OrdersRepositoryImpl(resolver.get(MainDbTokens.mainDbClient)),
-  ProductsRepository: (resolver: ResolverShape): ProductsRepository =>
-    new ProductsRepositoryImpl(resolver.get(MainDbTokens.mainDbClient)),
   TodoRepository: (resolver: ResolverShape): TodoRepository =>
     new TodoRepositoryImpl(resolver.get(MainDbTokens.mainDbClient)),
+  SessionsRepository: (resolver: ResolverShape): SessionsRepository =>
+    new SessionsRepositoryImpl(resolver.get(MainDbTokens.mainDbClient)),
 };
 
 const mainDbResources = defineResources(mainDbMap, {
