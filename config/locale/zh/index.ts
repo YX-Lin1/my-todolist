5eb992d0d1bc4eb2821c6bf8263cd0cdcab8bcce
YX-Lin1 <yuxuan.lin1@gientech.com>
2026-06-10 12:10:30 +0900

    feat(auth): Import login module into Chinese locale configuration

--- config/locale/zh/index.ts
+++ config/locale/zh/index.ts
@@ -2,6 +2,7 @@ import components from "./components";
 import demoUser from "./demo-user";
 import error from "./error";
 import homepage from "./homepage";
+import login from "./login";
 import seo from "./seo";
 import timezonesData, { regions } from "./timezones";
 import todolists from "./todolists";
@@ -12,6 +13,7 @@ export default {
   demoUser,
   error,
   components,
+  login,
   seo,
   timezones: {
     ...timezonesData,
